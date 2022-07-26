7056f925c6f3d5ebea47f7f5f08ff59182e6c720
Isargenys Contreras <48723570+Isargenys@users.noreply.github.com>
2022-07-26 13:16:41 -0400

    cambios

--- miperfil.php
+++ miperfil.php
@@ -6,6 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="css/miPerfil.css" />
+  <link rel="stylesheet" href="./assets/dashboard.css" />
+  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css" />
   <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
